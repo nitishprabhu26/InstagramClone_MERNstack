cf334cf7ffac240a0ce033b1e5b3c804666dc74e
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-03-26 15:15:58 -0500

    middleware setup, installed nodemon

--- server/app.js
+++ server/app.js
@@ -4,8 +4,34 @@ const app = express()
 // server will be listening on this port
 const PORT = process.env.PORT || 5000
 
+
+// custom middleware
+const customMiddleware = (req,res,next)=>{
+    console.log("middleware executed!!")
+    // will stop the current middleware and goes to next middleware 
+    // or 
+    // will stop the current middleware and execute the code further
+    next()
+}
+
+// all the routes will first go through the middleware, to make middleware run for all the routes defined for app
+// app.use(customMiddleware)
+
+
 // when user makes a get request
 app.get('/',(req,res)=>{
+    console.log("home")
+    res.send("Hello world")
+})
+
+// app.get('/about',(req,res)=>{
+//     console.log("about")
+//     res.send("Hello world")
+// })
+
+// To make middleware run for a particular route
+app.get('/about',customMiddleware,(req,res)=>{
+    console.log("about")
     res.send("Hello world")
 })
 
