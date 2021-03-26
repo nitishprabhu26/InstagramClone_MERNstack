efc5c255899c9f4e4a89f6565313df041117088a
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-03-26 16:24:01 -0500

    Setting up mongoDB on cloud using mongo.DB Atlas

--- server/app.js
+++ server/app.js
@@ -3,37 +3,57 @@ const express = require('express')
 const app = express()
 // server will be listening on this port
 const PORT = process.env.PORT || 5000
+// Install a package mongoose, which makes easier to perform queries and makes easier to communicate with mongoDB
+const mongoose = require('mongoose')
+
+const {MONGOURI} = require('./keys')
 
 
-// custom middleware
-const customMiddleware = (req,res,next)=>{
-    console.log("middleware executed!!")
-    // will stop the current middleware and goes to next middleware 
-    // or 
-    // will stop the current middleware and execute the code further
-    next()
-}
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
 
-// all the routes will first go through the middleware, to make middleware run for all the routes defined for app
-// app.use(customMiddleware)
-
-
-// when user makes a get request
-app.get('/',(req,res)=>{
-    console.log("home")
-    res.send("Hello world")
 })
 
-// app.get('/about',(req,res)=>{
+// callback function fires when successfully connected to database
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongo")
+})
+// on eror
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+// // custom middleware
+// const customMiddleware = (req,res,next)=>{
+//     console.log("middleware executed!!")
+//     // will stop the current middleware and goes to next middleware 
+//     // or 
+//     // will stop the current middleware and execute the code further
+//     next()
+// }
+
+// // all the routes will first go through the middleware, to make middleware run for all the routes defined for app
+// // app.use(customMiddleware)
+
+
+// // when user makes a get request
+// app.get('/',(req,res)=>{
+//     console.log("home")
+//     res.send("Hello world")
+// })
+
+// // app.get('/about',(req,res)=>{
+// //     console.log("about")
+// //     res.send("Hello world")
+// // })
+
+// // To make middleware run for a particular route
+// app.get('/about',customMiddleware,(req,res)=>{
 //     console.log("about")
 //     res.send("Hello world")
 // })
 
-// To make middleware run for a particular route
-app.get('/about',customMiddleware,(req,res)=>{
-    console.log("about")
-    res.send("Hello world")
-})
 
 // executes when server starts listening on this port
 app.listen(PORT,()=>{
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://nitish:Password123@cluster0.elrwb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
