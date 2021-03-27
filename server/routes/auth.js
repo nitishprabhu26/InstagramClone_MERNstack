e097fa2e2e131eac17327ca925ad17c98a7176ee
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-03-27 02:41:05 -0500

    posting data to database

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,44 @@
 const express = require('express')
 const router = express.Router()
 
+
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+
+
 // // basic route testing
 // router.get('/',(req,res)=>{
 //     res.send("hello")
 // })
 
+
 // signup routes
-router.post('/signup',(req,res)=>{
-    const {name,email,password}=req.body
-    if(!email || !password || !name){
-        res.status(422).json({error:"please add all the fields"})
+router.post('/signup', (req, res) => {
+    const { name, email, password, pic } = req.body
+    if (!email || !password || !name) {
+        return res.status(422).json({ error: "please add all the fields" })
     }
-    res.json({message:"successfully posted"})
-  })
+    User.findOne({ email: email })
+        .then((savedUser) => {
+            if (savedUser) {
+                return res.status(422).json({ error: "user already exists with that email" })
+            }
+            const user = new User({
+                email,
+                password,
+                name
+            })
+            user.save()
+                .then(user => {
+                    res.json({ message: "saved successfully" })
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
 
 module.exports=router
