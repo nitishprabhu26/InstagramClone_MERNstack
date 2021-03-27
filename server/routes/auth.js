9079a578218be46fce0ec497a7dc88cb202788a5
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-03-27 03:02:33 -0500

    created signin route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -46,4 +46,32 @@ router.post('/signup', (req, res) => {
         })
 })
 
+// signin route
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(422).json({ error: "please add email or password" })
+    }
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({ error: "Invalid Email or password" })
+            }
+            // doMatch is a boolean value
+            bcrypt.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        res.json({ message: "successfully signed in" })
+                    }
+                    else {
+                        return res.status(422).json({ error: "Invalid Email or password" })
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+})
+
+
 module.exports=router
