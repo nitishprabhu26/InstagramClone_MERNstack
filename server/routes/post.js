ff31936520ed88086a7224cc38ee6a35e37d61db
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-03-28 03:26:43 -0500

    created new route to view all posts

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,6 +4,22 @@ const mongoose = require('mongoose')
 const requireLogin  = require('../middleware/requireLogin')
 const Post =  mongoose.model("Post")
 
+// route to get all posts
+// since an id is stored in database(instead of the object) and hence, will send a response as objectid. but we want object 
+// .populate("parameter that we want to populate in the response","fields within that object, rest will be hidden")
+router.get('/allpost',(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id name")
+    .then((posts)=>{
+        res.json({posts})
+    }).catch(err=>{
+        console.log(err)
+    })
+    
+})
+
+
+// route to create a post
 router.post('/createpost',requireLogin,(req,res)=>{
     const {title,body,pic} = req.body 
     if(!title || !body ){
